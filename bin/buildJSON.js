8b8d6a71b7823f07b88ee17a66d7c2ea51249ba9
Drew Johnson <drew.johnson@jupiterone.com>
2022-11-02 18:07:35 -0400

    Testing new build config

--- bin/buildJSON.js
+++ bin/buildJSON.js
@@ -3,7 +3,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-import boards from "../dist/insights-dashboards.mjs";
+import boards from "../dist/index.mjs";
 
 fs.writeFileSync(
   `${__dirname}/../dist/boards.json`,
