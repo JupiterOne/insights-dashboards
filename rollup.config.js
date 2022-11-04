97813bd7e9f55e53cd551dec5922b5cba9f43e4c
Drew Johnson <drew.johnson@jupiterone.com>
2022-11-04 10:41:16 -0400

    Trying old build format

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,43 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
-import dts from "rollup-plugin-dts";
 import pkg from "./package.json";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
 export default [
+  // browser-friendly UMD build
   {
-    input: "tsc-dist/index.js",
-    output: [
-      {
-        file: pkg.main,
-        format: "cjs",
-        exports: "named",
-        sourcemap: true,
-      },
-      {
-        file: pkg.module,
-        format: "es",
-        exports: "named",
-        sourcemap: true,
-      },
-      {
-        file: pkg.browser,
-        format: "umd",
-        exports: "named",
-        sourcemap: true,
-        name: "InsightsDashboards",
-      },
-    ],
+    input: "src/index.ts",
+    output: {
+      name: "InsightsDashboards",
+      file: pkg.browser,
+      format: "umd",
+      sourcemap: true,
+    },
     plugins: [
       json(), // to parse json files
       resolve(), // so Rollup can find any dependencies
       commonjs(), // so Rollup can convert dependencies to ES modules
-      // typescript(),
+      typescript(),
     ],
   },
+
+  // CommonJS (for Node) and ES module (for bundlers) build
   {
-    input: "./tsc-dist/index.d.ts",
+    input: "src/index.ts",
+    output: [
+      { file: pkg.main, format: "cjs", sourcemap: true },
+      { file: pkg.module, format: "es", sourcemap: true },
+    ],
+    plugins: [
+      json(), // to parse json files
+      typescript(),
+    ],
+  },
+  // Definition file for TS support
+  {
+    input: "./dist/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "es" }],
     plugins: [dts()],
   },
